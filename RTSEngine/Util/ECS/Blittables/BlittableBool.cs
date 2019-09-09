0493c33e312dfd17c141aab360e5b9d9caa5c4f9
YanickZengaffinen <yanick.zengaffinen@gmail.com>
2019-09-09 21:19:13 +0200

    added blittable bool

---
+++ RTSEngine/Util/ECS/Blittables/BlittableBool.cs
@@ -0,0 +1,19 @@
+﻿public struct BlittableBool
+{
+    private readonly byte value;
+
+    public BlittableBool(bool value)
+    {
+        this.value = (byte)(value ? 1 : 0);
+    }
+
+    public static implicit operator BlittableBool(bool value)
+    {
+        return new BlittableBool(value);
+    }
+
+    public static implicit operator bool(BlittableBool value)
+    {
+        return value.value != 0;
+    }
+}
