67c9a6a3aa67817fecc83df73341e535edece941
YanickZengaffinen <yanick.zengaffinen@gmail.com>
2019-09-09 14:39:20 +0200

    rotation

--- RTSEngine/Camera/RTSCamera.cs
+++ RTSEngine/Camera/RTSCamera.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class RTSCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Transfor
-    }
+    [Header("Rotation")]
+    [SerializeField]
+    private KeyCode rotateKey = KeyCode.Mouse0;
+    [SerializeField]
+    private float rotationSpeed = 100;
 
-    // Update is called once per frame
+    private Vector3? lastMousePos;
+
     void Update()
     {
-        
+        if(lastMousePos != null)
+        {
+            HandleRotation();
+        }
+
+        lastMousePos = Input.mousePosition;
+    }
+
+    private void HandleRotation()
+    {
+        float rotationDelta = (Input.mousePosition.x - lastMousePos.Value.x) / Screen.width;
+
+        if (Input.GetKey(rotateKey))
+        {
+            transform.Rotate(Vector3.up, rotationDelta * rotationSpeed * Time.deltaTime);
+        }
     }
 }
